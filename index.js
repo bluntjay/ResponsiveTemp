45fe4f3045cb99926a1685d55a2ffb1d40202f86
AlexJ <43654720+bluntjay@users.noreply.github.com>
2020-02-11 15:30:19 +0200

    1st commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function openMenu() {
+  var nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+  var toggle = document.getElementById("toggle");
+  toggle.classList.toggle("toggle");
+
+  
+  
+}
